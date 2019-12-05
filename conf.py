557b1d5560860231d984a534fab05ecd320b7fb2
Xinrong Ding <xding78@gmail.com>
2019-12-05 16:16:59 +0100

    modify the config.py to use import sphinx_glpi_theme

--- conf.py
+++ conf.py
@@ -79,7 +79,10 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
+
+html_theme = "sphinx_glpi_theme"
+html_theme_path = ["_themes", ]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -179,8 +182,3 @@ epub_exclude_files = ['search.html']
 
 
 # -- Extension configuration -------------------------------------------------
-import sphinx_glpi_theme
-
-html_theme = "glpi"
-
-html_theme_path = [sphinx_glpi_theme.get_html_themes_path()]
